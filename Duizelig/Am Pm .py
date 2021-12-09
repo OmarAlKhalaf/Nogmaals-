ba69b0a09a3efb3b9a9b69f95f5440ed63b92b77
OmarAlKhalaf <99047548@mydavinci.nl>
2021-12-09 13:46:34 +0100

    (i add it a new map + i did finsh 2 task)

---
+++ Duizelig/Am Pm .py
@@ -0,0 +1,8 @@
+q = 0
+w = 12
+while q <= 11 :
+    print(q, "Am")
+    q = q +1
+while w <= 23 :
+    print(w, "Pm")
+    w = w + 1
---
+++ Duizelig/Raketlancering.py
@@ -0,0 +1,8 @@
+q = 30
+start = input("Als je wil raket te lancering type { go }.")
+if start == ("go"):
+    while q >= 1:
+        print("Raketlancer in " , q)
+        q=q-1
+    q = True
+    print("Raket has been launched")
